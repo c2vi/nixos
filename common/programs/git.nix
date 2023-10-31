a259b0ff3caade103c6d1ae18f822b66ee429471
Sebastian Moser <me@c2vi.dev>
2023-10-31 10:51:42 +0000

    git delta as diff output

--- common/programs/git.nix
+++ common/programs/git.nix
@@ -5,7 +5,11 @@
 		userName  = "Sebastian Moser";
 		userEmail = "me@c2vi.dev";
 	
-		extraConfig.core.editor = "nvim";
-		extraConfig.core.excludesfile = "~/.config/git/gitignore";
+		extraConfig = {
+			core.editor = "nvim";
+			core.color.ui = true;
+			core.pager = "delta";
+			extraConfig.core.excludesfile = "~/.config/git/gitignore";
+		};
 	};
 }
