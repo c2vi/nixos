e7e76fed186dde7b887d715edf8e39b6af70af4d
Sebastian Moser <me@c2vi.dev>
2023-10-31 18:10:14 +0100

    removed trace pring of self

--- common/nixos-graphical.nix
+++ common/nixos-graphical.nix
@@ -69,9 +69,7 @@
 
 	# xmonad
 	services.xserver.windowManager.xmonad = {
-   	#enable = true;
-   	#config = builtins.toPath "${self}/misc/xmonad.hs";
-		enable = builtins.trace "this is self ${self}" true;
+   	enable = true;
 		
    	config = builtins.readFile "${self}/misc/xmonad.hs";
    	#config = "${confDir}/misc/xmo";
