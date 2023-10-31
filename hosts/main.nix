aed46e41db21cf779720a01b04f0f07a9481e6d5
Sebastian Moser <me@c2vi.dev>
2023-10-31 10:47:28 +0000

    indentation fix in hosts/main.nix

--- hosts/main.nix
+++ hosts/main.nix
@@ -21,22 +21,23 @@
 		inputs.home-manager.nixosModules.home-manager
 		../users/me/home.nix
 	];
+
+	#home-manager.users.me = import ../users/me/home.nix;
 	
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
+  	# Setup keyfile
+  	boot.initrd.secrets = {
+    	"/crypto_keyfile.bin" = null;
+  	};
 
-  fileSystems."/home/me/work" =
-    { 
-      #label = "work";
-      device = "/dev/disk/by-uuid/fd3c6393-b6fd-4065-baf9-5690eb6ebbed";
-      fsType = "btrfs";
-      neededForBoot = false;
-    };
+  	fileSystems."/home/me/work" = { 
+		#label = "work";
+		device = "/dev/disk/by-uuid/fd3c6393-b6fd-4065-baf9-5690eb6ebbed";
+		fsType = "btrfs";
+		neededForBoot = false;
+	};
 
 
-  # Use the GRUB 2 boot loader.
+	# Use the GRUB 2 boot loader.
 	boot.loader.grub = {
   		enable = true;
   		version = 2;
@@ -46,16 +47,16 @@
 			set timeout=1
 		'';
 	};
-  boot.loader.efi.canTouchEfiVariables = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices = {
-    root = {
-      #name = "root";
-      device = "/dev/disk/by-uuid/142d2d21-2998-4eb7-9853-ab6554ba061f";
-      preLVM = true;
-      allowDiscards = true;
-    };
-  };
+	boot.initrd.luks.devices = {
+   	root = {
+   		#name = "root";
+      	device = "/dev/disk/by-uuid/142d2d21-2998-4eb7-9853-ab6554ba061f";
+      	preLVM = true;
+      	allowDiscards = true;
+   	};
+	};
 
 
 ############################# MISC #############################
@@ -73,53 +74,51 @@
 	};
 
 
-security.polkit.enable = true;
-networking.firewall.enable = true;
-networking.firewall.allowPing = true;
-services.samba.openFirewall = true;
+	security.polkit.enable = true;
+	networking.firewall.enable = true;
+	networking.firewall.allowPing = true;
+	services.samba.openFirewall = true;
 
-# samba
-services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
-networking.firewall.allowedTCPPorts = [
-  5357 # wsdd
-];
-networking.firewall.allowedUDPPorts = [
-  3702 # wsdd
-];
-services.samba = {
-  enable = true;
-  securityType = "user";
-  extraConfig = ''
-	security = user
-	map to guest = bad user
-	guest account = me
+	# samba
+	services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
+	networking.firewall.allowedTCPPorts = [
+  		5357 # wsdd
+	];
+	networking.firewall.allowedUDPPorts = [
+  		3702 # wsdd
+	];
+	services.samba = {
+  		enable = true;
+  		securityType = "user";
+  		extraConfig = ''
+			security = user
+			map to guest = bad user
+			guest account = me
 
-	server role = standalone server
-	workgroup = WORKGROUP
-  '';
-  shares = {
-    rpi_schule = {
-    path = "${workDir}/rpi-schule/";
-	 "guest ok" = "yes";
-    "read only" = "no";
-    public = "yes";
-    writable = "yes";
-    printable = "no";
-    comment = "share for rpi in school wlan";
-    };
+			server role = standalone server
+			workgroup = WORKGROUP
+  		'';
+  		shares = {
+    		rpi_schule = {
+    			path = "${workDir}/rpi-schule/";
+	 			"guest ok" = "yes";
+    			"read only" = "no";
+    			public = "yes";
+    			writable = "yes";
+    			printable = "no";
+    			comment = "share for rpi in school wlan";
+    		};
 
-    share = {
-    	comment = "share for sharing stuff";
-    	path = "${workDir}/share";
-    	public = "yes";
-	 	"guest ok" = "yes";
-    	"read only" = "no";
-    	writable = "yes";
-    };
-  };
-};
-###############
-
+    		share = {
+    			comment = "share for sharing stuff";
+    			path = "${workDir}/share";
+    			public = "yes";
+	 			"guest ok" = "yes";
+    			"read only" = "no";
+    			writable = "yes";
+    		};
+  		};
+	};
 
 
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -177,11 +176,11 @@ export PATH=$PATH:${confDir}/mybin
 		HibernateMode=shutdown
 	'';
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-   enable = true;
-   displayManager = {
-	defaultSession = "none+xmonad";
+	# Enable the X11 windowing system.
+	services.xserver = {
+		enable = true;
+   	displayManager = {
+		defaultSession = "none+xmonad";
    	sessionCommands = ''
 			xmobar ${confDir}/xmonad/xmobar.hs &
 
@@ -195,8 +194,8 @@ export PATH=$PATH:${confDir}/mybin
 				-e "add control = Control_L" \
 				-e "add mod1 = Alt_L" \
 				&
-     '';
-   };
+   	'';
+	};
 
    displayManager.lightdm = {
 		enable = true;
@@ -207,40 +206,39 @@ export PATH=$PATH:${confDir}/mybin
 				default-wallpaper=/usr/share/streets_of_gruvbox.png
 			'';
 		};
-   };
-   layout = "at";
-  };
+	};
+	layout = "at";
+	};
 
-  # Configure keymap in X11
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+	# Configure keymap in X11
+	# services.xserver.xkbOptions = "eurosign:e,caps:escape";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+	# Enable CUPS to print documents.
+	# services.printing.enable = true;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+	# Enable sound.
+	sound.enable = true;
+	hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+	# Enable touchpad support (enabled default in most desktopManager).
+	services.xserver.libinput.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix!
-    wget
-    xorg.xmodmap
-	 bluez
-  ];
+	# List packages installed in system profile. To search, run:
+	# $ nix search wget
+	environment.systemPackages = with pkgs; [
+   	vim # Do not forget to add an editor to edit configuration.nix!
+   	wget
+   	xorg.xmodmap
+		bluez
+	];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It's perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "23.05"; # Did you read the comment?
 }
 
 
