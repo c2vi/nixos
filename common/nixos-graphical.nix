2fd2b3d492da92323152158c8f9d08f47d8a9bdc
Sebastian Moser <me@c2vi.dev>
2023-10-31 16:34:24 +0000

    even more more more more fixes for hpm

--- common/nixos-graphical.nix
+++ common/nixos-graphical.nix
@@ -73,7 +73,7 @@
    	#config = builtins.toPath "${self}/misc/xmonad.hs";
 		enable = builtins.trace "this is self ${self}" true;
 		
-   	config = "${self}" + /misc/xmonad.hs;
+   	config = builtins.readFile "${self}/misc/xmonad.hs";
    	#config = "${confDir}/misc/xmo";
    	enableContribAndExtras = true;
    	extraPackages = hpkgs: [
