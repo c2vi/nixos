79f525022fb4020b217ad60cca581499eba8cf2b
Sebastian Moser <me@c2vi.dev>
2023-11-22 18:14:33 +0100

    ...

--- flake.nix
+++ flake.nix
@@ -168,16 +168,27 @@
         modules = [
           #./users/me/headless.nix
           ({ pkgs, ... }: {
-            users.users.nix-on-droid.openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFjgXf9S9hxjyph2EEFh1el0z4OUT9fMoFAaDanjiuKa me@main"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWsqiz0gEepvPONYxqhKKq4Vxfe1h+jo11k88QozUch me@bitwarden"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAioUu4ow6k+OMjjLdzogiQM4ZEM3TNekGNasaSDzQQE me@phone"
-            ];
             environment.packages = with pkgs; [ 
               vim
               openssh
             ];
             system.stateVersion = "23.05";
+            home-manager.extraSpecialArgs = {
+              inherit self;
+              hostname = "phone";
+            };
+
+            home-manager.config = {
+              home.stateVersion = "22.11";
+              home.file.".ssh/authorizedKeys".text = ''
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFjgXf9S9hxjyph2EEFh1el0z4OUT9fMoFAaDanjiuKa me@main"
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWsqiz0gEepvPONYxqhKKq4Vxfe1h+jo11k88QozUch me@bitwarden"
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAioUu4ow6k+OMjjLdzogiQM4ZEM3TNekGNasaSDzQQE me@phone"
+              '';
+              imports = [
+                #./users/common/home.nix
+              ];
+            };
           })
         ];
       };
