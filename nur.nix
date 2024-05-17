c668e309cd7a9ce649ae1d4e14aff466af0c6bfb
Sebastian Moser <sebastian@c2vi.dev>
2024-05-17 12:47:49 +0200

    added iio-hyprland to nur

--- nur.nix
+++ nur.nix
@@ -2,6 +2,16 @@
 # Reminder for myself: Any package here should not import <nixpkgs>, but use the pkgs
 
 { pkgs ? import <nixpkgs> { } }: {
+
   cbm = pkgs.callPackage ./mods/cbm.nix {};
+
   mac-telnet = pkgs.callPackage ./mods/mac-telnet.nix {};
+
+  iio-hyprland = let
+    repo = pkgs.fetchFromGitHub {
+      owner = "yassineibr";
+      repo = "iio-hyprland";
+      rev = "nix-support";
+      hash = "sha256-xFc8J8tlw6i+FbTC05nrlvQIXRmguFzDqh+SQOR54TE=";
+    }; in pkgs.callPackage "${repo}/default.nix" {};
 }
