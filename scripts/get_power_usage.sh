bab6f2f8467f77cc2c9d36d126adee8d51cbd514
lostmia <testemail429455@gmail.com>
2025-03-21 13:25:26 +0100

    Fixed Battery not showing up

--- scripts/get_power_usage.sh
+++ scripts/get_power_usage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-power=$(cat /sys/class/power_supply/BAT1/current_now /sys/class/power_supply/BAT1/voltage_now | xargs | awk '{ printf "%.1f\n", $1 * $2 / 1e12 }')
+power=$(cat /sys/class/power_supply/BAT*/current_now /sys/class/power_supply/BAT*/voltage_now | xargs | awk '{ printf "%.1f\n", $1 * $2 / 1e12 }')
 
 echo $power
