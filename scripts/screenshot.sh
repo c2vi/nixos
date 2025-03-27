2bdcfe66956246da8e50d202497c1d5639d55da6
Sebastian Moser <sebastian@c2vi.dev>
2025-03-27 19:31:14 +0100

    new screenshot script

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,1 +1,14 @@
-flameshot gui -r | wl-copy
+hyprpicker -r -z &
+
+geometry=$(slurp -c '#ff0000ff')
+
+if [[ "$?" != "0" ]]
+then
+  pkill hyprpicker || true
+  exit
+fi
+
+
+grim -g "$geometry" -t ppm - | satty --filename - --copy-command=wl-copy --early-exit &
+
+pkill hyprpicker || true
